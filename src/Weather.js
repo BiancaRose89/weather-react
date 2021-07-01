e65fcda29c8b2143fabb3d73f54c83be2ddf6c39
BiancaRose89 <77932470+BiancaRose89@users.noreply.github.com>
2021-07-01 13:10:15 +0100

    Changed the loader to deeppink

---
+++ src/Weather.js
@@ -0,0 +1,27 @@
+import React from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The Weather in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+
+  let apiKey = "e36aa7e435ba36b070815e93be179f97";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <div>
+      <Loader
+        type="Puff"
+        color="deeppink"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      />
+    </div>
+  );
+}
