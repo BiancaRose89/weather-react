481aa720a1fd279922744f2f52d4638ed06aea14
BiancaRose89 <77932470+BiancaRose89@users.noreply.github.com>
2021-07-03 06:41:32 +0100

    Reorganised & added the Github Repository

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Weather from "./Weather";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <Weather />
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by
+        <a
+          href="https://adoring-kepler-a9a12f.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Bianca Schwartz{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/BiancaRose89/weather-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub{" "}
+        </a>
+        and{" "}
+        <a
+          href="https://focused-bardeen-30372a.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify{" "}
+        </a>
+      </footer>
     </div>
   );
 }
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
