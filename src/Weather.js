d36edd58269cbb64069b189732553f5a6e7bd89a
BiancaRose89 <77932470+BiancaRose89@users.noreply.github.com>
2021-07-03 06:15:49 +0100

    Added the footer to link to GitHub & Netlify

--- src/Weather.js
+++ src/Weather.js
@@ -64,4 +64,30 @@ export default function Weather() {
       </div>
     </div>
   );
+  <footer>
+    This project was coded by
+    <a
+      href="https://adoring-kepler-a9a12f.netlify.app/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Bianca Schwartz
+    </a>
+    and is
+    <a
+      href="https://github.com/BiancaRose89/weather-react"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      open-sourced on GitHub
+    </a>
+    and
+    <a
+      href="https://focused-bardeen-30372a.netlify.app/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      hosted on Netlify
+    </a>
+  </footer>;
 }
