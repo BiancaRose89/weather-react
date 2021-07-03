87315d82f49c492282cacafc324da6dee1095e16
BiancaRose89 <77932470+BiancaRose89@users.noreply.github.com>
2021-07-03 06:53:26 +0100

    Added Bootstrap to my Project

--- src/App.js
+++ src/App.js
@@ -5,33 +5,36 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by
-        <a
-          href="https://adoring-kepler-a9a12f.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Bianca Schwartz{" "}
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/BiancaRose89/weather-react"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub{" "}
-        </a>
-        and{" "}
-        <a
-          href="https://focused-bardeen-30372a.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hosted on Netlify{" "}
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by
+          <a
+            href="https://adoring-kepler-a9a12f.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Bianca Schwartz{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/BiancaRose89/weather-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub{" "}
+          </a>
+          and{" "}
+          <a
+            href="https://focused-bardeen-30372a.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
